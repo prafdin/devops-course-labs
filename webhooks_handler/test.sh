4516307f1c45405d29031aecc396a814eb46d567
APMorozov <morozov-sasha200@yandex.ru>
2026-02-28 19:16:40 +0000

    Добавил скрипт для запуска тестов

---
+++ webhooks_handler/test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+APP_DIR="/home/alex/3kurs/dev-ops/catty-reminders-app"
+VENV="$APP_DIR/venv"
+
+echo "=== Запуск тестов проекта ==="
+
+cd "$APP_DIR"
+
+if [ -f "requirements.txt" ]; then
+    if [ ! -d "venv" ]; then
+        echo "=== создание виртуального окружения $BRANCH ==="
+        python -m venv venv
+    fi
+    echo "=== запуск виртуального окружения $BRANCH ==="
+    source venv/bin/activate
+    echo "=== установка зависимостей $BRANCH ==="
+    pip install -r requirements.txt
+fi
+
+if [ -f "requirements.txt" ]; then
+    echo "=== Установка зависимостей ==="
+    pip install -r requirements.txt
+fi
+
+echo "=== Выполняем тесты из папки tests ==="
+pytest tests --maxfail=1 --disable-warnings -q
+
+RESULT=$?
+
+exit $RESULT
