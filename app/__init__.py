1a6b2fcdd613bb3924e0873f113a5014ef5796e2
Maximord <m4ximyarosl@yandex.ru>
2026-03-16 12:00:36 -0700

    Fix: read deploy_ref from file instead of env var

--- app/__init__.py
+++ app/__init__.py
@@ -21,7 +21,11 @@ with open('config.json') as config_json:
   users = config['users']
   db_path = config['db_path']
 
-DEPLOY_REF = os.getenv("DEPLOY_REF", "NA")
+try:
+    with open('/opt/catty-reminders/deploy_ref.txt', 'r') as f:
+        DEPLOY_REF = f.read().strip()
+except:
+    DEPLOY_REF = "NA"
 
 # --------------------------------------------------------------------------------
 # Establish the Secret Key
