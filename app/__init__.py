2fffec59d4b18b986b3469cd78ba1305bdcdd399
Maximord <m4ximyarosl@yandex.ru>
2026-03-16 14:45:41 -0700

    Use commit hash for deploy_ref

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ This module builds shared parts for other modules.
 
 import json
 import os
-import datetime
+import subprocess
 from fastapi.templating import Jinja2Templates
 
 # Читаем конфигурацию
@@ -13,8 +13,22 @@ with open('/opt/catty-reminders/config.json') as config_json:
     users = config['users']
     db_path = config['db_path']
 
-# DEPLOY_REF - время последнего деплоя
-DEPLOY_REF = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+# DEPLOY_REF - хэш текущего коммита
+try:
+    result = subprocess.run(
+        ["git", "rev-parse", "--short", "HEAD"],
+        cwd="/opt/catty-reminders",
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+        timeout=2
+    )
+    if result.returncode == 0:
+        DEPLOY_REF = result.stdout.strip()
+    else:
+        DEPLOY_REF = "NA"
+except:
+    DEPLOY_REF = "NA"
 
 # Секретный ключ
 secret_key = config['secret_key']
