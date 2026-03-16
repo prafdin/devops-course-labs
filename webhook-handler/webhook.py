aa9f72c67c865331077baba05ab1c0ce3a646bd6
Maximord <m4ximyarosl@yandex.ru>
2026-03-17 00:59:57 +0400

    Update webhook.py

--- webhook-handler/webhook.py
+++ webhook-handler/webhook.py
@@ -77,10 +77,10 @@ def restart_app(commit_hash=None):
         if check.stdout and check.stdout.strip() == "200":
             logging.info(f"Приложение успешно перезапущено и отвечает 200")
 
-	    import datetime
-	    with open('/opt/catty-reminders/deploy_time.txt', 'w') as f:
+	        import datetime
+	        with open('/opt/catty-reminders/deploy_time.txt', 'w') as f:
                 f.write(datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
-	    logging.info(f"⏱️ Deploy time записан")
+	        logging.info(f"⏱️ Deploy time записан")
 
             return True
         else:
