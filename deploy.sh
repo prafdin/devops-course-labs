2468a92fb83cac0d8c5c1935ca9f299d7b6b99c7
only-hell <dnslodygin20032005@gmail.com>
2026-03-29 18:17:55 +0400

    Add armored deploy.sh with logging

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,28 @@
+exec > /home/vboxuser/catty-reminders-app/deploy.log 2>&1
+echo "=== Deploy started at $(date) ==="
+
 cd /home/vboxuser/catty-reminders-app
 
 
+git config --global --add safe.directory /home/vboxuser/catty-reminders-app
+
+echo "Fetching origin..."
 git fetch origin
+
+echo "Resetting to origin/lab1..."
 git reset --hard origin/lab1
 
-
+echo "Pulling latest changes..."
 git pull origin lab1
 
+NEW_HEAD=$(git rev-parse HEAD)
+echo "New HEAD is: $NEW_HEAD"
 
-echo "DEPLOY_REF=$(git rev-parse HEAD)" > .env
-
+echo "DEPLOY_REF=$NEW_HEAD" > .env
+echo "File .env updated."
 
 ./build.sh
 ./test.sh
 
-
 sudo systemctl restart catty-app
+echo "=== Deploy finished successfully ==="
