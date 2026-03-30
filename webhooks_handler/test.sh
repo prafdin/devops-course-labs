e8da957ea046bbc6da8e87a60dfde5f27b6d5b17
APMorozov <morozov-sasha200@yandex.ru>
2026-03-30 16:18:47 +0000

    Переаботал под запуска на VM

--- webhooks_handler/test.sh
+++ webhooks_handler/test.sh
@@ -1,57 +1,39 @@
 #!/bin/bash
 set -e
-APP_DIR="."
-VENV="$APP_DIR/venv"
 
-echo "=== Запуск тестов проекта ветки $BRANCH==="
+echo "=== Запуск тестов ==="
+
+cd "$(dirname "$0")/.."
 
 DEPLOY_REF="$(git rev-parse HEAD)"
-
-echo "Текущий SHA ветки $BRANCH: $DEPLOY_REF"
 echo "DEPLOY_REF=$DEPLOY_REF" > .env.deploy
 
+echo "Текущий SHA: $DEPLOY_REF"
 
-if [ -f "requirements.txt" ]; then
-    if [ ! -d "venv" ]; then
-        echo "=== создание виртуального окружения $BRANCH ==="
-        python -m venv venv
-    fi
-    echo "=== запуск виртуального окружения $BRANCH ==="
-    source venv/bin/activate
-    echo "=== установка зависимостей $BRANCH ==="
-    pip install -r requirements.txt
+if [ ! -d "venv" ]; then
+    echo "=== создание venv ==="
+    python -m venv venv
 fi
 
-if [ -f "requirements.txt" ]; then
-    echo "=== Установка зависимостей ==="
-    pip install -r requirements.txt
-fi
+source venv/bin/activate
+
+echo "=== установка зависимостей ==="
+pip install -r requirements.txt
 
 
-echo "Текущая директория: $(pwd)"
-if [ -f "requirements.txt" ]; then
-    if [ ! -d "venv" ]; then
-        echo "=== создание виртуального окружения $BRANCH ==="
-        python -m venv venv
-    fi
-    echo "=== запуск виртуального окружения $BRANCH ==="
-    source venv/bin/activate
-    echo "=== установка зависимостей $BRANCH ==="
-    pip install -r requirements.txt
-fi
-
-echo "=== запуск приложения $BRANCH   !===" 
 set -a
-source $APP_DIR/.env.deploy
+source .env.deploy
 set +a
-nohup uvicorn app.main:app --reload --host 0.0.0.0 --port 8181 > /dev/null 2>&1 &
-APP_PID=$!
 
-sleep 3
+echo "=== запуск pytest ==="
 
-echo "=== Выполняем тесты из папки tests ==="
-pytest tests --maxfail=1 --disable-warnings -q
+export PYTHONPATH=$PWD
+
+pytest tests \
+  --maxfail=1 \
+  --disable-warnings \
+  -q
+
 RESULT=$?
-kill $APP_PID
 
 exit $RESULT
