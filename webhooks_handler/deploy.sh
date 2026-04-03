67374974003ac8a5bd7a478b08c269296237fe1d
APMorozov <morozov-sasha200@yandex.ru>
2026-04-03 08:52:41 +0000

    начал принимать хэш ветки релиза

--- webhooks_handler/deploy.sh
+++ webhooks_handler/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-BRANCH=$1
+DEPLOY_REF=$1
 REPO_URL="git@github.com:APMorozov/catty-reminders-app"
 APP_DIR="/home/alex/3kurs/dev-ops/catty-reminders-app"
 
 
-echo "=== DEPLOY ветки $BRANCH ==="
+echo "=== DEPLOY релиза ==="
 
 if [ ! -d "$APP_DIR/.git" ]; then
     echo "Первый запуск — клонирование"
@@ -15,27 +15,26 @@ fi
 
 cd $APP_DIR
 
-git fetch origin
+echo "=== Подтягиваем обновления ==="
+git fetch --all --tags
 
-git checkout -B $BRANCH origin/$BRANCH
+echo "=== Переходим на  commit ==="
+git checkout --detach $DEPLOY_REF
 
-git reset --hard origin/$BRANCH
 
-DEPLOY_REF="$(git rev-parse HEAD)"
-
-echo "Текущий SHA ветки $BRANCH: $DEPLOY_REF"
+echo "Текущий SHA релиза: $DEPLOY_REF"
 echo "DEPLOY_REF=$DEPLOY_REF" > /home/alex/3kurs/dev-ops/catty-reminders-app/.env.deploy
 
 if [ -f "requirements.txt" ]; then
     if [ ! -d "venv" ]; then
-        echo "=== создание виртуального окружения $BRANCH ==="
+        echo "=== создание виртуального окружения==="
         python -m venv venv
     fi
-    echo "=== запуск виртуального окружения $BRANCH ==="
+    echo "=== запуск виртуального окружения ==="
     source venv/bin/activate
-    echo "=== установка зависимостей $BRANCH ==="
+    echo "=== установка зависимостей ==="
     pip install -r requirements.txt
 fi
 
-echo "=== запуск приложения $BRANCH ==="
+echo "=== запуск приложения ==="
 sudo systemctl restart myapp
