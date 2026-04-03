73596cb0c740ff51e99f89a2ce1863e9ab8d8e3a
sofiastepanova <20sophi05@gmail.com>
2026-04-04 02:26:39 +0400

    testim pipelines

---
+++ run_app.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# 1. Заходим в папку
+cd /home/ilya/catty-reminders-app || exit 1
+
+SHA=$1
+echo "Deploying SHA: $SHA"
+
+# 2. код из GitHub
+git fetch --all
+git reset --hard "$SHA"
+
+# 3. зависимости
+/home/password-123/catty-reminders-app/venv/bin/python -m pip install -r requirements.txt
+
+# 4. Сохраняем DEPLOY_REF
+echo "DEPLOY_REF=$SHA" | sudo tee /etc/catty-app-env
+
+# 5. Перезапускаем
+sudo systemctl restart catty-app
+
+# 6. Проверка
+sleep 3
+if systemctl is-active --quiet catty-app
+    echo "SUCCESS: Deployed $SHA"
+else
+    echo "ERROR"
+    exit 1
+fi
