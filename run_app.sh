d283db0e3fc190a3a2f1ca177284e3974c65b69e
password_123 <password_123@vbox.www.digmawifi.com>
2026-04-04 02:14:04 +0400

    Lab2: Final setup for CI/CD

---
+++ run_app.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+cd /home/ilya/catty-reminders-app || exit 1
+
+SHA=$1
+echo "Deploying SHA: $SHA"
+
+
+git fetch --all
+git reset --hard "$SHA"
+
+
+/home/ilya/catty-reminders-app/venv/bin/python -m pip install -r requirements.txt
+
+
+echo "DEPLOY_REF=$SHA" | sudo tee /etc/catty-app-env
+
+
+sudo systemctl restart catty-app
+
+sleep 3
+if systemctl is-active --quiet catty-app; then
+    echo "SUCCESS: Deployed $SHA"
+else
+    echo "ERROR: App failed"
+    exit 1
+fi
