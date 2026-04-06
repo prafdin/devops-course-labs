eb127556361019e235017a6eab2047068fed6b0e
password_123 <password_123@vbox.www.digmawifi.com>
2026-04-07 01:08:03 +0400

    testik

--- run_app.sh
+++ run_app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ilya/catty-reminders-app || exit 1
+cd /home/password_123/catty-reminders-app || exit 1
 
 SHA=$1
 echo "Deploying SHA: $SHA"
@@ -9,16 +9,16 @@ git fetch --all
 git reset --hard "$SHA"
 
 
-/home/ilya/catty-reminders-app/venv/bin/python -m pip install -r requirements.txt
+/home/password_123/catty-reminders-app/venv/bin/python -m pip install -r requirements.txt
 
 
 echo "DEPLOY_REF=$SHA" | sudo tee /etc/catty-app-env
 
 
-sudo systemctl restart catty-app
+sudo systemctl restart catty
 
 sleep 3
-if systemctl is-active --quiet catty-app; then
+if systemctl is-active --quiet catty; then
     echo "SUCCESS: Deployed $SHA"
 else
     echo "ERROR: App failed"
