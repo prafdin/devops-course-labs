3690197675ac5e01dfada6b75cede6772e6fa575
kirill <kirillshtengauer@gmail.com>
2026-04-09 19:43:49 +0400

    fix deploy.sh again

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
 
-# === Параметры ===
+IMAGE_NAME="$1"
+DEPLOY_REF="$2"
+IMAGE_FULL="${IMAGE_NAME}:${DEPLOY_REF}"
+
 APP_DIR="/home/kirill/desktop/devops"
 ENV_FILE="$APP_DIR/.env.deploy"
-IMAGE_FULL="$1:$2"
 CONTAINER_NAME="catty-reminders-app"
 PORT=8181
 
 echo "Deploying: $IMAGE_FULL"
+echo "DEPLOY_REF: $DEPLOY_REF"
 
 echo "Cleaning..."
 docker stop "$CONTAINER_NAME" 2>/dev/null || true
@@ -19,11 +22,10 @@ sudo fuser -k "$PORT/tcp" 2>/dev/null || true
 echo "Pulling..."
 docker pull "$IMAGE_FULL"
 
-DEPLOY_REF="$2"
 printf 'DEPLOY_REF=%s\n' "$DEPLOY_REF" > "$ENV_FILE"
 echo "Saved DEPLOY_REF=$DEPLOY_REF to $ENV_FILE"
 
-echo "tarting container..."
+echo "Starting container..."
 docker run -d \
     -p "$PORT":"$PORT" \
     --name "$CONTAINER_NAME" \
