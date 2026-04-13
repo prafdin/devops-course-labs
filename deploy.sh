b9956d687b9122ab75bba8da8077f97bef41abaa
only-hell <dnslodygin20032005@gmail.com>
2026-04-13 20:49:27 +0400

    Fix Lab 2: Correct Dockerfile syntax and fix FRP port

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 cd /home/vboxuser/catty-reminders-app
-
 COMMIT_SHA=$1
-if[ -z "$COMMIT_SHA" ]; then
+if [ -z "$COMMIT_SHA" ]; then
   COMMIT_SHA=$(git rev-parse HEAD)
 fi
 echo "DEPLOY_REF=$COMMIT_SHA" > .env
 
-# Останавливаем старые версии (на всякий случай)
+# Очищаем старые контейнеры, если они зависли
 sudo systemctl stop catty-app 2>/dev/null || true
-docker stop lab2-app 2>/dev/null || true
-docker rm lab2-app 2>/dev/null || true
+docker compose down 2>/dev/null || true
+docker rm -f lab2-app 2>/dev/null || true
 
-# Собираем и запускаем
+# Собираем и запускаем!
 docker build -t lab2-app-image .
 docker run -d --name lab2-app --restart always -p 8181:8181 --env-file .env lab2-app-image
