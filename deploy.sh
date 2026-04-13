bee690267f85626afb93a31e8539c80d065cb3b0
only-hell <dnslodygin20032005@gmail.com>
2026-04-13 21:54:06 +0400

    Fix Lab 2: Force remove existing container

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,7 @@ if [ -z "$COMMIT_SHA" ]; then
   COMMIT_SHA=$(git rev-parse HEAD)
 fi
 echo "DEPLOY_REF=$COMMIT_SHA" > .env
-
-# Очищаем старые контейнеры, если они зависли
-sudo systemctl stop catty-app 2>/dev/null || true
-docker compose down 2>/dev/null || true
+docker stop lab2-app 2>/dev/null || true
 docker rm -f lab2-app 2>/dev/null || true
-
-# Собираем и запускаем!
 docker build -t lab2-app-image .
 docker run -d --name lab2-app --restart always -p 8181:8181 --env-file .env lab2-app-image
