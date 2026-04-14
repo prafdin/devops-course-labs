513b4c2b10d88305781ec2afd0b3bcc238b4dc67
Maximord <m4ximyarosl@yandex.ru>
2026-04-14 11:14:33 -0700

    Remove playwright from conftest, keep only unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,72 +1,45 @@
-"""
-This module provides fixtures for testing.
-
-WARNING:
-Why don't we just use command line arguments for base URL and passwords?
-Because it's annoying to type them out every time we want to run tests!
-
-TODO: storage state for UI and API
-"""
-
-# --------------------------------------------------------------------------------
-# Imports
-# --------------------------------------------------------------------------------
-
-import json
 import pytest
+from typing import Dict, Any
+import json
+import os
 
-from playwright.sync_api import Playwright
-from testlib.inputs import User
-
-
-# --------------------------------------------------------------------------------
-# Private Functions
-# --------------------------------------------------------------------------------
-
-def _build_user(inputs, index):
-  assert 'users' in inputs, "inputs are missing 'users' key"
-  users = inputs['users']
-
-  assert len(users) > index, f"index {index} is out of range for input 'users'"
-  assert 'username' in users[index], f"input 'users[{index}]' is missing 'username'"
-  assert 'password' in users[index], f"input 'users[{index}]' is missing 'password'"
-  user = User(users[index]['username'], users[index]['password'])
-
-  return user
-
-
-# --------------------------------------------------------------------------------
-# Input Fixtures
-# --------------------------------------------------------------------------------
-
-@pytest.fixture(scope='session')
-def test_inputs():
-  with open('inputs.json') as inputs_json:
-    data = json.load(inputs_json)
-  return data
-
-
-@pytest.fixture(scope='session')
-def base_url(test_inputs):
-  assert 'base_url' in test_inputs, "inputs are missing 'base_url' key"
-  return test_inputs['base_url']
-
-
-@pytest.fixture(scope='session')
-def user(test_inputs):
-  return _build_user(test_inputs, 0)
-
-
-@pytest.fixture(scope='session')
-def alt_user(test_inputs):
-  return _build_user(test_inputs, 1)
-
-
-# --------------------------------------------------------------------------------
-# Playwright Fixtures
-# --------------------------------------------------------------------------------
+def load_test_data():
+    """Load test data from inputs.json"""
+    try:
+        with open('/opt/catty-reminders/inputs.json', 'r') as f:
+            return json.load(f)
+    except:
+        return {"users": [{"username": "test", "password": "test"}]}
 
 @pytest.fixture
-def catty_api(playwright: Playwright, base_url: str):
-  return playwright.request.new_context(base_url=base_url)
+def user():
+    """Return test user"""
+    data = load_test_data()
+    user_data = data.get('users', [{}])[0]
+    
+    class User:
+        def __init__(self, username, password):
+            self.username = username
+            self.password = password
+    
+    return User(
+        username=user_data.get('username', 'test'),
+        password=user_data.get('password', 'test')
+    )
 
+@pytest.fixture
+def alt_user():
+    """Return alternative test user"""
+    data = load_test_data()
+    users = data.get('users', [{}])
+    user_data = users[1] if len(users) > 1 else users[0]
+    
+    class User:
+        def __init__(self, username, password):
+            self.username = username
+            self.password = password
+    
+    return User(
+        username=user_data.get('username', 'test2'),
+        password=user_data.get('password', 'test2')
+    )
