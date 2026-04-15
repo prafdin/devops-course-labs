da15d3ae367e5f55b6c70aae94aa8f7d7b937ecc
Maximord <m4ximyarosl@yandex.ru>
2026-04-14 18:42:53 -0700

    Resolve merge conflict and fix templates path

--- app/__init__.py
+++ app/__init__.py
@@ -4,18 +4,22 @@ This module builds shared parts for other modules.
 
 import json
 import os
-import subprocess
 from fastapi.templating import Jinja2Templates
 
+# Определяем базовую директорию
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 # Читаем конфигурацию
-with open('/opt/catty-reminders/config.json') as config_json:
+config_path = os.path.join(BASE_DIR, 'config.json')
+with open(config_path) as config_json:
     config = json.load(config_json)
     users = config['users']
     db_path = config['db_path']
 
 # DEPLOY_REF - читаем из файла, который обновляет CD
+deploy_ref_path = '/opt/catty-reminders/deploy_ref.txt'
 try:
-    with open('/opt/catty-reminders/deploy_ref.txt', 'r') as f:
+    with open(deploy_ref_path, 'r') as f:
         DEPLOY_REF = f.read().strip()
 except:
     DEPLOY_REF = "NA"
@@ -23,5 +27,6 @@ except:
 # Секретный ключ
 secret_key = config['secret_key']
 
-# Шаблоны
-templates = Jinja2Templates(directory="/opt/catty-reminders/templates")
+# Шаблоны - используем относительный путь
+templates_dir = os.path.join(BASE_DIR, "templates")
+templates = Jinja2Templates(directory=templates_dir)
