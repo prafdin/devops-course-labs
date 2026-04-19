e4b6eabb15faa92668f85bec4c53ebeeb04a7670
Deemeed <dmitrijcencov82500@gmail.com>
2026-04-19 22:11:29 +0400

    Container deploy in CD

--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,21 @@
 echo "Запуск деплоя catty-reminders-app..."
 echo "======================================"
 
-REPO_DIR=$(pwd) 
-SERVICE_NAME="catty-app"
-
 DEPLOY_REF=$1
-#DEPLOY_REF="$(git rev-parse HEAD)"
+IMAGE_NAME="ghcr.io/deemeed/catty-reminders-app:$DEPLOY_REF"
 
 echo "Текущий SHA ветки: $DEPLOY_REF"
-echo "DEPLOY_REF=$DEPLOY_REF" > /home/deemeed/catty-reminders-app/.env
 
-echo "Перезапуск сервиса..."
-sudo systemctl daemon-reload
-sudo systemctl enable $SERVICE_NAME
-sudo systemctl restart $SERVICE_NAME
+docker pull $IMAGE_NAME
 
-cd $REPO_DIR
+docker stop catty-container 2>/dev/null || true
+docker rm catty-container 2>/dev/null || true
+
+docker run -d \
+    --name catty-container \
+    -p 8181:8181 \
+    --restart unless-stopped \
+    -e DEPLOY_REF=$DEPLOY_REF \
+    $IMAGE_NAME
 
 echo "Деплой успешно завершен!"
