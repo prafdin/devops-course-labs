14772806e84176cc507033b3429f4b7f5f2085e9
RenTogan <rmansyr06@gmail.com>
2026-05-02 13:33:35 +0000

    chore: revert changes in init and login to match main

--- app/__init__.py
+++ app/__init__.py
@@ -21,7 +21,7 @@ with open('config.json') as config_json:
   users = config['users']
   db_path = config['db_path']
 
-DEPLOY_REF = os.getenv("DEPLOY_REF", "NA")
+DEPLOY_REF = os.getenv("DEPLOY_COMMIT", "NA")
 
 # --------------------------------------------------------------------------------
 # Establish the Secret Key
