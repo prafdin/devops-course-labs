5377801b526864901ac85cbe36e7b2a18f71805a
IvanDelibaltov <e.kartash@yandex.ru>
2026-05-03 11:19:40 +0000

    lab3: docker ci/cd

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+IMAGE_NAME=$1
+DEPLOY_REF=$2
+HOST_PORT=8181
+CONTAINER_PORT=8181
+CONTAINER_NAME="catty-reminders-app"
+IMAGE="$IMAGE_NAME:$DEPLOY_REF"
+
+echo "=== DEPLOY релиза ==="
+echo "Текущий SHA релиза: $DEPLOY_REF"
+
+docker pull $IMAGE
+docker stop $CONTAINER_NAME || true
+docker rm $CONTAINER_NAME || true
+
+docker run -d \
+    -p $HOST_PORT:$CONTAINER_PORT \
+    --name $CONTAINER_NAME \
+    --restart unless-stopped \
+    $IMAGE
