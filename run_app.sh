ef9e66bc8c7021fd76ac6e34e7cf7a6bd49f052c
chernikova <1vistu1@gmail.com>
2026-05-04 15:16:39 +0000

    Fix deploy config like working example

--- run_app.sh
+++ run_app.sh
@@ -7,21 +7,11 @@ echo "Deploying SHA: $SHA"
 git fetch --all
 git reset --hard "$SHA"
 
-if [ ! -d "venv" ]; then
-    echo "Creating venv..."
-    python3 -m venv venv
-fi
+/home/vboxuser/lab2/catty-reminders-app/venv/bin/python -m pip install -r requirements.txt
 
-./venv/bin/python -m pip install -r requirements.txt
-
-# Обновляем DEPLOY_REF в файле
 echo "DEPLOY_REF=$SHA" | sudo tee /etc/catty-app-env
 
-# Перезапускаем приложение
 sudo systemctl restart catty-app
-
-# Перезагружаем Nginx, чтобы подхватил новую переменную
-sudo systemctl reload nginx
 
 sleep 3
 if systemctl is-active --quiet catty-app; then
