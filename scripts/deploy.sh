ff7c1f3e56eeb53989939cb3dea09a1c9f988fa3
vboxuser <vboxuser@debian.myguest.virtualbox.org>
2026-05-08 18:22:59 +0400

    trigger deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,48 +1,37 @@
 #!/bin/bash
-
 set -e
 
-REPO_LOWER=$(echo "$REPO_NAME" | tr '[:upper:]' '[:lower:]')
-
+REPO_LOWER=$(echo "$REPO" | tr '[:upper:]' '[:lower:]')
 IMAGE="ghcr.io/${REPO_LOWER}:${RELEASE_HASH}"
 
-echo "Deploying image:"
-echo "$IMAGE"
+echo "Deploying: $IMAGE"
 
-ssh -p "${DEPLOY_PORT}" \
-    -o StrictHostKeyChecking=no \
-    "${DEPLOY_USER}@${DEPLOY_HOST}" << EOF
+ssh -i ~/.ssh/id_rsa \
+  -o StrictHostKeyChecking=no \
+  "${DEPLOY_USER}@${DEPLOY_HOST}" << EOF
 
 set -e
 
-echo "> Login to GHCR"
+echo "Login GHCR"
+echo "${GH_TOKEN}" | docker login ghcr.io \
+  -u "${GH_USER}" \
+  --password-stdin
 
-echo "${DOCKER_TOKEN}" | docker login ghcr.io \
-    -u "${GITHUB_ACTOR}" \
-    --password-stdin
-
-echo "> Pull image"
-
+echo "Pull image"
 docker pull ${IMAGE}
 
-echo "> Stop old container"
-
+echo "Stop old container"
 docker stop catty-app || true
 docker rm catty-app || true
 
-echo "> Start new container"
-
+echo "Start new container"
 docker run -d \
-    --name catty-app \
-    --restart unless-stopped \
-    -p 8181:8181 \
-    -e COMMIT_SHA=${RELEASE_HASH} \
-    ${IMAGE}
-
-sleep 5
+  --name catty-app \
+  --restart unless-stopped \
+  -p 8181:8181 \
+  -e COMMIT_SHA=${RELEASE_HASH} \
+  ${IMAGE}
 
 docker ps
-
-echo "> Deploy successful"
-
+echo "DEPLOY OK"
 EOF
