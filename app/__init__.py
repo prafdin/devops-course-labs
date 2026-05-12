8157485d63db900a0b7c812f72215db682ff61e3
Андрей <@email.com>
2026-05-12 18:45:57 +0400

    use docker secrets for mysql password

--- app/__init__.py
+++ app/__init__.py
@@ -20,6 +20,10 @@ with open('config.json') as config_json:
   config = json.load(config_json)
   users = config['users']
   db_config = config['db_config']
+  password_file = db_config.pop('password_file', None)
+  if password_file and os.path.exists(password_file):
+    with open(password_file) as db_password:
+      db_config['password'] = db_password.read().strip()
 
 DEPLOY_REF = os.getenv("DEPLOY_REF", "NA")
 
