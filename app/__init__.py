e72839e34925c82599c4ce60fa1c880df3a205f5
Andrey Kruglov <andrey@example.com>
2026-05-14 17:55:02 -0400

    Restore app/__init__.py to its original state as requested

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,37 @@
-import os
+"""
+This module builds shared parts for other modules.
+"""
+
+# --------------------------------------------------------------------------------
+# Imports
+# --------------------------------------------------------------------------------
+
 import json
-import subprocess
+import os
+
 from fastapi.templating import Jinja2Templates
 
+
+# --------------------------------------------------------------------------------
+# Read Configuration
+# --------------------------------------------------------------------------------
+
 with open('config.json') as config_json:
-    config = json.load(config_json)
-    users = config['users']
-    db_path = config['db_path']
+  config = json.load(config_json)
+  users = config['users']
+  db_path = config['db_path']
 
 DEPLOY_REF = os.getenv("DEPLOY_REF", "NA")
+
+# --------------------------------------------------------------------------------
+# Establish the Secret Key
+# --------------------------------------------------------------------------------
 
 secret_key = config['secret_key']
-templates = Jinja2Templates(directory="templates")
 
-DEPLOY_REF = os.getenv("DEPLOY_REF", "NA")
-    if DEPLOY_REF != "NA":
-        return DEPLOY_REF
-    try:
-            ['git', 'rev-parse', '--short', 'HEAD'], 
-            cwd=os.path.dirname(os.path.abspath(__file__))
-        ).decode('ascii').strip()
-    except Exception:
-        return "NA"
+
+# --------------------------------------------------------------------------------
+# Templates
+# --------------------------------------------------------------------------------
+
+templates = Jinja2Templates(directory="templates")
