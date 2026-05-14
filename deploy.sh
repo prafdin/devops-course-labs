3241f09a9dd578dcadb8d37b33e12d22fb3837a8
Косцов Владимир <131604064+NITSENSE@users.noreply.github.com>
2026-05-14 21:51:22 +0400

    fix

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,5 @@
 cd /home/enjoyer/my-app
 git fetch origin lab1
 git reset --hard origin/lab1
-CURRENT_SHA=$(git rev-parse HEAD)
-echo "DEPLOY_REF=$CURRENT_SHA" > /home/enjoyer/my-app/.env
-echo "DEPLOYREF=$CURRENT_SHA" >> /home/enjoyer/my-app/.env
 ./venv/bin/pip install -r requirements.txt
 sudo systemctl restart catty-app
