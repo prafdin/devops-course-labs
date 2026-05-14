e0eb7a3299f124aa1805bfe680d165091439a7ba
Косцов Владимир <131604064+NITSENSE@users.noreply.github.com>
2026-05-14 20:12:37 +0400

    Minor changes

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd /home/enjoyer/my-app
 git pull origin lab1
+CURRENT_SHA=$(git rev-parse HEAD)
+echo "DEPLOY_REF=$CURRENT_SHA" > /home/enjoyer/my-app/.env
+echo "DEPLOYREF=$CURRENT_SHA" >> /home/enjoyer/my-app/.env
 ./venv/bin/pip install -r requirements.txt
 sudo systemctl restart catty-app
-echo "Deploy finished at $(date)"
