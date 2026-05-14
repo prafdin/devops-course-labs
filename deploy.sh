672f66ff7a44c56f56b3921280d4e3314b672480
Косцов Владимир <131604064+NITSENSE@users.noreply.github.com>
2026-05-14 20:30:12 +0400

    Minor changes

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /home/enjoyer/my-app
-git pull origin lab1
+git fetch origin lab1
+git reset --hard origin/lab1
 CURRENT_SHA=$(git rev-parse HEAD)
 echo "DEPLOY_REF=$CURRENT_SHA" > /home/enjoyer/my-app/.env
 echo "DEPLOYREF=$CURRENT_SHA" >> /home/enjoyer/my-app/.env
