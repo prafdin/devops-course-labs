d6da764b3afdf14d9653269c975efc899e6bef90
chernikova <1vistu1@gmail.com>
2026-05-16 20:16:23 +0000

    Fix __init__.py to use db_config instead of db_path

--- app/__init__.py
+++ app/__init__.py
@@ -1,37 +1,17 @@
-"""
-This module builds shared parts for other modules.
-"""
-
-# --------------------------------------------------------------------------------
-# Imports
-# --------------------------------------------------------------------------------
+"""This module builds shared parts for other modules."""
 
 import json
 import os
-
 from fastapi.templating import Jinja2Templates
 
-
-# --------------------------------------------------------------------------------
 # Read Configuration
-# --------------------------------------------------------------------------------
-
 with open('config.json') as config_json:
-  config = json.load(config_json)
-  users = config['users']
-  db_config = config['db_config']
+    config = json.load(config_json)
+    users = config.get('users', {})
+    db_config = config.get('db_config', {})
+    secret_key = config.get('secret_key', 'default-secret-key')
 
 DEPLOY_REF = os.getenv("DEPLOY_REF", "NA")
 
-# --------------------------------------------------------------------------------
-# Establish the Secret Key
-# --------------------------------------------------------------------------------
-
-secret_key = config['secret_key']
-
-
-# --------------------------------------------------------------------------------
 # Templates
-# --------------------------------------------------------------------------------
-
 templates = Jinja2Templates(directory="templates")
