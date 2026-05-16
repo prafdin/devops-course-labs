8f09f55687b5cb7fcf74aa6266529997015a2141
Stanislav Kuzmin <a663ve@gmail.com>
2026-05-16 16:22:42 +0400

    new file deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+REPO_DIR="/home/qzm/Desktop/catty-reminders-app"
+TARGET_COMMIT=$1
+
+echo "> Deploying commit $TARGET_COMMIT"
+
+cd "$REPO_DIR"
+
+sudo git reset --hard HEAD || true
+
+sudo git fetch origin
+sudo git checkout -f "$TARGET_COMMIT"
+
+DEPLOY_REF=$(git rev-parse HEAD)
+echo "DEPLOY_REF=$DEPLOY_REF" | sudo tee "$REPO_DIR/.env.deploy"
+sudo chmod 644 "$REPO_DIR/.env.deploy"
+sudo chown qzm:qzm "$REPO_DIR/.env.deploy"
+
+echo "> Deploy ref updated to: $DEPLOY_REF"
+
+if [ ! -d ".venv" ]; then
+    echo "> Virtual environment was not found, creating..."
+    python3 -m venv .venv
+fi
+
+source .venv/bin/activate
+
+if [ -f "requirements.txt" ]; then
+    echo "> Installing/Updating requirements"
+    pip install --upgrade pip
+    pip install -r requirements.txt
+fi
+
+echo "> Restarting app.service..."
+sudo systemctl daemon-reload
+sudo systemctl restart app.service
+echo "> Done!"
