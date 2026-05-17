ef742d02a06915323589cd939014361b74cd6046
Shureevk <164297560+Shureevk@users.noreply.github.com>
2026-05-17 19:20:22 +0400

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Deploying $1"
-echo "DEPLOY_REF=$1" | sudo tee /etc/catty-app-env
-sudo systemctl daemon-reload
-sudo systemctl restart catty
-echo "Deployed ref: $(cat /etc/catty-app-env)"
