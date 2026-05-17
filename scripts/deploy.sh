1f4668d19785ca96c38d7aa6ed53b4837409bb21
vboxuser <vboxuser@debian.myguest.virtualbox.org>
2026-05-17 19:41:15 +0400

    add deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,8 +19,6 @@ if [ ! -f "docker-compose.yaml" ] && [ -f "docker-compose.yml" ]; then
     COMPOSE_FILE="docker-compose.yml"
 fi
 
-echo "$GHCR_TOKEN" | docker login ghcr.io -u "$GHCR_USER" --password-stdin
-
 cat > .env << EOL
 RELEASE_HASH=$RELEASE_HASH
 MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD
@@ -28,9 +26,17 @@ MYSQL_USER=$MYSQL_USER
 MYSQL_PASSWORD=$MYSQL_PASSWORD
 EOL
 
+echo "$GHCR_TOKEN" | docker login ghcr.io -u "$GHCR_USER" --password-stdin
+
 docker-compose -f $COMPOSE_FILE --env-file .env down || true
 docker-compose -f $COMPOSE_FILE --env-file .env pull
 docker-compose -f $COMPOSE_FILE --env-file .env up -d --force-recreate
+
+echo "Waiting for database initialization..."
+sleep 10
+
+docker-compose -f $COMPOSE_FILE --env-file .env up -d catty_backend
+
 docker image prune -af || true
 docker ps
 EOF
