2ae5305845ac27772f4be1482753155821c5f762
vboxuser <vboxuser@debian.myguest.virtualbox.org>
2026-05-17 17:21:02 +0400

    add deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,7 @@ set -e
 
 APP_DIR="/home/vboxuser/catty-reminders-app"
 TARGET_PORT="${SERVER_PORT:-22}"
+
 REPO_LOWER=$(echo "$REPO_NAME" | tr '[:upper:]' '[:lower:]')
 
 echo "Deploying..."
@@ -15,14 +16,21 @@ set -e
 
 cd $APP_DIR
 
-echo "RELEASE_HASH=$RELEASE_HASH" > .env
-echo "REPO_LOWER=$REPO_LOWER" >> .env
+cat > .env << EOL
+RELEASE_HASH=$RELEASE_HASH
+REPO_LOWER=$REPO_LOWER
+EOL
+
+echo "$GHCR_TOKEN" | docker login ghcr.io -u "$GHCR_USER" --password-stdin
 
 docker-compose --env-file .env down || true
+
 docker-compose --env-file .env pull
+
 docker-compose --env-file .env up -d --force-recreate
 
 docker image prune -af || true
+
 docker ps
 
 EOF
