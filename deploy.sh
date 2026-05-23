562a4e647528809f0c840bcd87ac269b93e8aa61
Alexander <qwerty5480.89@gmail.com>
2026-05-23 17:21:11 +0400

    Add webhook server, deploy script and systemd services

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "=== Starting deployment ==="
+cd /home/alexander/csharp-example
+
+echo "1. Pulling latest changes from GitHub..."
+git pull origin lab1
+
+echo "2. Running unit tests..."
+dotnet test
+if [ $? -eq 0 ]; then
+    echo "Tests passed! 🟢"
+    echo "3. Building and publishing the application..."
+    dotnet publish -c Release -o /home/alexander/app ExampleWebService/ExampleWebService.csproj
+    
+    # 3.1 Получаем хэш последнего коммита и записываем его в файл окружения
+    COMMIT_HASH=$(git rev-parse HEAD)
+    echo "DEPLOY_REF=$COMMIT_HASH" > /home/alexander/app/csharp.env
+    echo "Saved commit hash ($COMMIT_HASH) to environment file."
+    
+    echo "4. Restarting the web application service..."
+    sudo /usr/bin/systemctl restart csharp-app.service
+    echo "=== Deployment finished successfully! 🚀 ==="
+else
+    echo "Tests failed! 🔴 Deployment aborted."
+    exit 1
+fi
