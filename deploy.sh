78feafc992981706f3d4ac0e0498f0e1ebed8355
Stanislav Kuzmin <a663ve@gmail.com>
2026-05-24 19:36:47 +0400

    fix script 2

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 set -e
 
-if [[ -z "$SERVER_HOST" || -z "$SERVER_USER" || -z "$RELEASE_HASH" || -z "$REPO_NAME" ]]; then
-    exit 1
-fi
+APP_DIR="/home/qzm/catty-compose"
 
-REPO_LOWER=$(echo "$REPO_NAME" | tr '[:upper:]' '[:lower:]')
+echo "Deploying..."
 
-ssh -p "${SERVER_PORT:-22}" -o StrictHostKeyChecking=no "${SERVER_USER}@${SERVER_HOST}" << REMOTE_SCRIPT
-    cd /home/qzm/catty-compose
-    
-    echo "REPO_LOWER=${REPO_LOWER}" > .env
-    echo "RELEASE_HASH=${RELEASE_HASH}" >> .env
-    
+ssh -o StrictHostKeyChecking=no "${SERVER_USER}@${SERVER_HOST}" << REMOTE_SCRIPT
+    cd ${APP_DIR}
+    docker compose down --remove-orphans
     docker compose pull
-    docker compose up -d --remove-orphans --force-recreate
-    
+    docker compose up -d
     docker image prune -af
 REMOTE_SCRIPT
