9f5cb9ed73123ba343834b0a951933f9d84c3356
Лиза К. <lizakolganowa@gmail.com>
2026-05-24 18:14:49 +0000

    test

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+BRANCH="${1:-lab1}"
 cd "$HOME/catty-reminders-app"
 
 git fetch origin
 
-git reset --hard origin/lab1
+git checkout -B "$BRANCH" "origin/$BRANCH"
+
+git reset --hard "origin/$BRANCH"
 
 sudo systemctl restart catty
