7990d3fc6d08e03a89121fad26116097c0a314be
light <light@Puk>
2026-05-26 02:10:31 +0400

    CI/CD edited

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+set -e
+
+echo "Создаем изолированное окружение для тестов..."
+python3 -m venv .temp_venv
+source .temp_venv/bin/activate
+
+echo "Устанавливаем зависимости..."
+pip install -r requirements.txt
+
+echo "Запускаем pytest..."
+# Запускаем тесты
+python3 -m pytest tests/
+
+echo "Все тесты успешно пройдены!"
