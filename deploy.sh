a30c00394adbacf13b8e3acba37e1a704ccb2b8b
Stanislav Kuzmin <a663ve@gmail.com>
2026-05-27 16:52:57 +0400

    add removing orhans

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,6 @@ set -e
 cd /home/qzm/Desktop/catty-reminders-app
 
 docker compose pull
+docker compose down --volumes --remove-orphans || true
+docker rm -f catty_db catty_app || true
 docker compose up -d --remove-orphans
