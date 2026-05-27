7ef0833c55af59d78b066ea7d9c682185fbb8f9a
Stanislav Kuzmin <a663ve@gmail.com>
2026-05-27 16:23:07 +0400

    delete symbols

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ APP_DIR="/home/qzm/Desktop/catty-reminders-app"
 
 ssh -p "$SERVER_PORT" -o StrictHostKeyChecking=no "${SERVER_USER}@${SERVER_HOST}" "bash -s" << REMOTE_SCRIPT
     set -e
-    export REPO_LOWER=\"${REPO_LOWER}\"
-    export RELEASE_HASH=\"${RELEASE_HASH}\"
+    export REPO_LOWER="${REPO_LOWER}\"
+    export RELEASE_HASH="${RELEASE_HASH}\"
 
     cd ${APP_DIR}
     
