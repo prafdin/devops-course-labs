9242dccbe38745fc737da3620b9a5a55fd9cf15e
Stanislav Kuzmin <a663ve@gmail.com>
2026-05-27 16:32:00 +0400

    fix work with env

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 set -e
 
-APP_DIR="/home/qzm/Desktop/catty-reminders-app"
+cd /home/qzm/Desktop/catty-reminders-app
 
-ssh -p "$SERVER_PORT" -o StrictHostKeyChecking=no "${SERVER_USER}@${SERVER_HOST}" "bash -s" << REMOTE_SCRIPT
-    set -e
-    echo "REPO_LOWER=$REPO_LOWER" > .env
-    echo "RELEASE_HASH=$RELEASE_HASH" >> .env
-
-    cd ${APP_DIR}
-    
-    echo \"Stopping old stack\"
-    docker compose down || true
-    
-    echo \"Pulling images\"
-    docker compose pull
-    
-    echo \"Starting new stack\"
-    docker compose up -d
-    
-    docker image prune -af
-REMOTE_SCRIPT
+docker compose pull
+docker compose up -d --remove-orphans
