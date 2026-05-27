3739ad641c19cd988a0a6322aed0e9f3bdb17abd
Stanislav Kuzmin <a663ve@gmail.com>
2026-05-27 16:19:47 +0400

    fixes in deploy

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 set -e
 
-APP_PATH="/home/qzm/Desktop/catty-reminders-app"
-cd $APP_PATH
+APP_DIR="/home/qzm/Desktop/catty-reminders-app"
 
-export REPO_LOWER="kuzminstanislav/catty-reminders-app"
-export RELEASE_HASH=$(git rev-parse HEAD)
+ssh -p "$SERVER_PORT" -o StrictHostKeyChecking=no "${SERVER_USER}@${SERVER_HOST}" "bash -s" << REMOTE_SCRIPT
+    set -e
+    export REPO_LOWER=\"${REPO_LOWER}\"
+    export RELEASE_HASH=\"${RELEASE_HASH}\"
 
-echo "REPO_LOWER=${REPO_LOWER}" > .env
-echo "RELEASE_HASH=${RELEASE_HASH}" >> .env
-
-docker compose pull
-docker compose up -d --remove-orphans --pull always
+    cd ${APP_DIR}
+    
+    echo \"Stopping old stack\"
+    docker compose down || true
+    
+    echo \"Pulling images\"
+    docker compose pull
+    
+    echo \"Starting new stack\"
+    docker compose up -d
+    
+    docker image prune -af
+REMOTE_SCRIPT
