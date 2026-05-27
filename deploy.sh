ad66dd67d89ca12c7e093294ed1e606114a1a5f0
Stanislav Kuzmin <a663ve@gmail.com>
2026-05-27 16:12:35 +0400

    fixes with env

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 set -e
 
-if [[ -z "$REPO_LOWER" || -z "$RELEASE_HASH" ]]; then
-    echo "Ошибка: Переменные REPO_LOWER или RELEASE_HASH не установлены."
-    exit 1
-fi
+APP_PATH="/home/qzm/Desktop/catty-reminders-app"
+cd $APP_PATH
 
-ssh -p "$SERVER_PORT" -o StrictHostKeyChecking=no "${SERVER_USER}@${SERVER_HOST}" << REMOTE_SCRIPT
-    set -e
-    cd /home/${SERVER_USER}/Desktop/catty-reminders-app
+export REPO_LOWER="kuzminstanislav/catty-reminders-app"
+export RELEASE_HASH=$(git rev-parse HEAD)
 
-    echo "REPO_LOWER=${REPO_LOWER}" > .env
-    echo "RELEASE_HASH=${RELEASE_HASH}" >> .env
+echo "REPO_LOWER=${REPO_LOWER}" > .env
+echo "RELEASE_HASH=${RELEASE_HASH}" >> .env
 
-    docker compose pull
-    docker compose up -d --remove-orphans --pull always
-REMOTE_SCRIPT
+docker compose pull
+docker compose up -d --remove-orphans --pull always
