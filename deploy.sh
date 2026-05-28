d736bc06933c5098cb721eec705f3a9901fbf30c
Stanislav Kuzmin <a663ve@gmail.com>
2026-05-28 17:01:30 +0400

    add scrypt

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 set -e
 
-cd /home/qzm/Desktop/catty-reminders-app
+export REPO_LOWER=$(echo "${{ github.repository }}" | tr '[:upper:]' '[:lower:]')
+
+cd /home/qzm/Desktop/catty-reminders-app/
+
+echo "${{ secrets.GITHUB_TOKEN }}" | docker login ghcr.io -u ${{ github.actor }} --password-stdin
 
 docker compose pull
-docker compose down --volumes --remove-orphans || true
-docker ps -a --filter "name=catty" -q | xargs -r docker rm -f
-docker compose up -d --remove-orphans
-docker system prune -f
+docker compose up -d --remove-orphans --force-recreate
+
+sleep 5
+
+if [ $(docker inspect -f '{{.State.Running}}' catty_backend) == "true" ]; then
+    exit 0
+else
+    docker logs catty_backend
+    exit 1
+fi
