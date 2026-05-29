6c28348d80a1ded0da41959231fe2bf0d511eef4
Velesikamid <Velesikamid05082005@gmail.com>
2026-05-29 06:57:19 +0400

    started lab2

--- webhook/args.sh
+++ webhook/args.sh
@@ -4,3 +4,26 @@ PROXY=course.prafdin.ru
 TOKEN=devops
 ID=kiselev
 SSH_PORT=3146
+
+sudo tee /etc/frp/frpc.toml > /dev/null <<EOF
+serverAddr = "$PROXY"
+serverPort = 7000
+auth.method = "token"
+auth.token = "$TOKEN"
+
+[[proxies]]
+name = "app-$ID"
+type = "http"
+localPort = 8181
+customDomains = ["app.$ID.$PROXY"]
+
+[[proxies]]
+name = "ssh-$ID"
+type = "tcp"
+localPort = 22
+remotePort = $SSH_PORT
+EOF
+
+sudo systemctl restart frpc
+
+ssh -p $SSH_PORT $USER@$PROXY
